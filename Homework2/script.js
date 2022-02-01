97035a635527f7bbb1241d71c9db0b2f81a924c6
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-01-31 19:03:15 -0700

    HW2

---
+++ Homework2/script.js
@@ -0,0 +1,12 @@
+// 3 favorite websites
+console.log("My 3 favorite websites are Instagram, Adobe, and Netflix.");
+
+console.log('');
+
+// 3 favorite games
+console.log('My 3 favorite games are Connect4, Trouble, and Life')
+
+console.log('');
+
+// 3 favorite artists
+console.log('I feel like my 3 favorite artists sound kind of unoriginal, Van Gogh, Da Vinci, and Michelangelo, but their art was beautiful and they were so influential to the history and development of art. Van gogh was a Post-Impressionist artist in the mid-1800s. His paintings that used bold colors, and expressive impulsive brush strokes, started to pave the way for Expressionism. Da Vinci was an artist in the middle ages, and is best known for his artworks Mona Lisa, and The Last Supper,.they are known and loved around the world. Da Vinci had a huge influence on the start of the Renaissance with his incredibly realistic, almost lifelike, paintings. Michelangelo was also an artist during the Renaissance. He is very well known for painting the ceiling of the Sistine Chapel. The piece was 133ft long by 46ft wide. The piece was huge and did not lack detail anywhere, it is jaw dropping. The way that Michelangelo depected these figures with such an exaggerated approch, making them aesthetically pleasing, helped influence the Mannerism period. All of these artists and their artwork are so impressively beautiful, and important to art history. ');
