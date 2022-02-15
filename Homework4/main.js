711ddd62853a3268e5f5f438f2ff74b7a72e5da5
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-02-15 00:32:58 -0700

    Delete main.js

--- Homework4/main.js
+++
@@ -1,6 +0,0 @@
-function getName(greeting)
-            {
-                var currentName = document.getElementById("txtName").value;
-                document.getElementById("lblName").innerHTML = ;
-
-            }
