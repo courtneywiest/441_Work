562b64b3f82d36d7b32723f4170b22efd67c1a8d
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-02-21 22:47:05 -0700

    trying - failing - confused

--- Homework5/script.js
+++ Homework5/script.js
@@ -1,56 +1,26 @@
-//array of image names corresponding to image tags
-var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
-var blankImagePath = "/.images/cardboard.pdf";
-
+var images = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+var backside = "./images/cardback.pdf"
 var actualImages = new Array();
+
 
 function printBlanks()
 {
-   // call our random image creation function
-    createRandomImageArray();
-    // create a for loop
-    for(var i = 0; i < imageTags.length; i++)
-    {
-    // iterate through the image tag ids and sets the source
-        document.getElementById(imageTags[i]).src= blankImagePath;
-    }
+  RandomImageArray();
 
+    for(var i = 0; i < images.length; i++)
+      {
 
-
+      document.getElementById(images[i]).src= backside;
+      }
 }
 
-function createRandomImageArray()
-{
-    // create an array of actual images
-    var actualImagePath = ["./images/1-sleepytongue.jpeg", "./images/2-sandysherm.jpeg", "./images/3-crossedpaws.jpeg", "./images/4-goofysherm.jpeg" "./images/5-toy.jpeg", "./images/6-snowy.jpeg", "./images/7-birthday.jpeg", "./images/8-sneakay.jpg", "./images/9-lipies.jpeg", "./images/10-couchboi.jpeg",];
-    // create another array to make sure the images only get added twice
-    var count = [0,0];
-    // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 10)
-    {
-        // get a random number between 0 and the number total number of images that we can choose from
-        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
-        if(count[randomNumber] < 2)
-        {
-            actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
-            count[randomNumber] = count[randomNumber] + 1;
-        }
-    }
+ function RandomImageArray()
+ {
+   var actualImagePath = ["./images/1-sleepytongue.jpeg", "./images/2-sandysherm.jpeg", "./images/3-crossedpaws.jpeg", "./images/4-goofysherm.jpeg", "./images/5-toy.jpeg", "./images/6-snowy.jpeg", "./images/7-birthday.jpeg", "./images/8-sneakay.jpg", "./images/9-lipies.jpeg", "./images/10-couchboi.jpeg"];
 
+   var count = [0,0];
 
+   while(actualImages.length < 10)
+   
 
-
-
-}
-
-function flipImage(number)
-{
-    document.getElementById(imageTags[number]).src= actualImages[number];
-        // this should be a quick function that just changes
-        // the image based on what number was pressed
-
-
-}
+ }
