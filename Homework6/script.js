d083587c440ff34194ada50fb6adf234b3ec1cc2
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-02-27 15:57:05 -0700

    more hw6

--- Homework6/script.js
+++ Homework6/script.js
@@ -2,8 +2,10 @@ var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "im
 var blankImagePath = "images/dogpoker.jpeg";
 var firstNumber = -1;
 var secondNumber = -1;
+var score = 0;
+var allFound = 0;
 
-var player = {"firstname":"", "lastname":""};
+var player = {"firstname":"", "lastname":"", "age":0, "score":0};
 
 
 var actualImages = new Array();
@@ -31,7 +33,7 @@ while(actualImages.length < 10)
 {
   var randomNumber = Math.floor(Math.random() * actualImagePath.length)
 
-  if(count[randomNumber] < 2)
+  if(count[randomNumber] < 3)
   {
     actualImages.push(actualImagePath[randomNumber]);
 
@@ -60,13 +62,24 @@ else if(firstNumber < 0)
 
 if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
 {
+  score++;
   setTimeout(imagesDisappear, 1000);
 }
 
 else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
   {
+    score++;
+    allFound++;
+
     firstNumber = -1;
     secondNumber = -1;
+
+    if(allFound == actualImages.length/2)
+    {
+      player.score = score;
+      localStorage.setItem("playerInfo", JSON.stringify(player));
+      window.location = "indexEnd.html"
+    }
   }
 
 }
@@ -86,8 +99,12 @@ function imagesDisappear()
 function addToPlayer()
 {
   var firstName = document.getElementById("txtFirstName").value;
+  var lastName = document.getElementById("txtLastName").value;
+  var age = document.getElementById("txtAge").value;
 
   player.firstname = firstName;
+  player.lastname = lastName;
+  player.age = age;
   localStorage.setItem("playerInfo", JSON.stringify(player));
   window.location = "index.html";
 }
@@ -97,5 +114,11 @@ function playerInfo()
 {
   var playerInformation = localStorage.getItem("playerInfo");
   player = JSON.parse(playerInformation);
-  console.log(player.firstname);
+
+  var str = "Name: " + player.firstname + " " + player.lastname + "<br>" + "Age: " + player.age + "<br>" + "Score: " + player.score;
+
+  if (document.getElementById("endInformation") != null)
+  {
+    document.getElementById("endInformation").innerHTML = str;
+  }
 }
