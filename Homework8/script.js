eb03a28b1d3191f9164ffb72c85d74fc3c3d2ee1
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-03-14 22:29:56 -0600

    did iiiitttttttt

--- Homework8/script.js
+++ Homework8/script.js
@@ -1,64 +1,18 @@
-var dogSelector = "#dog";
-var allDogs = new Array();
-class DogInfo{
-    constructor(selector, imagePath)
-    {
-        this.selector = selector;
-        this.imagePath = imagePath;
-    }
-
-    get theSelector(){
-        return this.selector;
-    }
-
-    get theImagePath(){
-        return this.imagePath;
-    }
-
-    toString()
-    {
-        return this.selector + ":" + this.imagePath;
-    }
-}
 
 function initializeArray()
 {
+var randomnumber=Math.floor(Math.random()*$("#container").children().length);
+$(function() {
+    //hide all the images (if not already done)
+    $("#container > img").hide();
 
-    var dog = new DogInfo("#dog", "./images/brooks.jpeg", "./images/hawaiisunset.jpeg", "./images/mashouse.jpeg", "./images/sherm.jpeg", "./images/southhills.jpeg", "./images/staplessunset.jpeg");
-    allDogs.push(dog);
-
-
-}
-
-$(document).ready(function(){
-    initializeArray();
-    console.log(allDogs.length);
-    console.log(allDogs[0].toString());
-    console.log(allDogs[0].theSelector);
-    console.log(allDogs[0].theImagePath);
-
-    $(allDogs[0].theSelector).src = allDogs[0].theImagePath;
-    $("#my_p").attr("src","second.jpg");
-    $(allDogs[0].theSelector).attr("src", allDogs[0].theImagePath);
-
-    $("button").click(function(){
-
-        $(".stuff").fadeOut();
-
-        $("#third").toggle();
-           setInterval(moveSquare, 1000);
-
-        $(allDogs[0].theSelector).fadeOut().fadeIn();
-
+    //set timeout for image to appear (set at 500ms)
+    setTimeout(function (){
+       //fade in the random index of the image collection
+       $("#container > img:eq(" + randomnumber + ")").fadeIn("slow");
     });
-
 });
-
-function getRandomImage() {
-  var randomNumber = Math.random() * allDogs.length;
 }
-
-
 
 function moveSquare()
 {
