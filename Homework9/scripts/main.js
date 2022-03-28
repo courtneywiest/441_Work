ef28b2096d51f476f46cbd9bd7e774f0eb870697
Courtney Wiest <cw118679@umconnect.umt.edu>
2022-03-27 21:39:41 -0600

    more hw 9

--- Homework9/scripts/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-        $.getJSON("./data/jsondata.json", function(data){
-            console.log(data.name); // Prints: Harry
-            console.log(data.age); // Prints: 14
-        });
-        });
-    
